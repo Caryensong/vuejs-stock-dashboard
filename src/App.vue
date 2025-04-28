484da1e2755960fb820fdeb39d9f9fd03c23c660
Caryensong <caryensong@googlemail.com>
2025-04-28 09:10:58 +0200

    first component created

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
-   <div id="app">
-    Hello World
-  </div>
+    <BaseCard>
+      <h2>Erste Karte</h2>
+      <p>Dies ist der Inhalt der ersten Card.</p>
+    </BaseCard>
 </template>
 
 <script>
-
+import BaseCard from './components/BaseCard.vue';
 
 export default {
+  
   name: 'App',
   components: {
-  
+    BaseCard
   }
 }
 </script>
@@ -19,8 +21,11 @@ export default {
 body{
   margin: 0;
 }
+
 #app {
   background: radial-gradient(71.11% 100% at 50% 0%, #020204 14.6%, #011F35 100%);
+  padding: 100px;
+  box-sizing: border-box;
   width: 100vw;
   min-height: 100vh;
 }
---
+++ src/components/BaseCard.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="card">
+      <slot></slot>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'BaseCard'
+  }
+  </script>
+  
+  <style scoped>
+  .card {
+    background: #011F35;
+    width: 714px;
+    height: 352px;
+    gap: 20px;
+    border-radius: 16px;
+    padding: 20px 32px;
+    box-sizing: border-box;
+  }
+  </style>
+  
